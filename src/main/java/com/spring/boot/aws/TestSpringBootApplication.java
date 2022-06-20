0f5431d960fae89d357915097e7efe13b47d1fef
Rahulpersie66 <Rahulpersie66@gmail.com>
2022-06-21 00:34:41 +0530

    Aws files deploy

---
+++ src/main/java/com/spring/boot/aws/TestSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.spring.boot.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
