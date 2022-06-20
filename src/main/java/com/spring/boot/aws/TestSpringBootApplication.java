044a7cdff1b982183e4bf42845107d4011b92770
Rahulpersie66 <Rahulpersie66@gmail.com>
2022-06-21 01:06:16 +0530

    Aws files deploy docker file

--- src/main/java/com/spring/boot/aws/TestSpringBootApplication.java
+++ src/main/java/com/spring/boot/aws/TestSpringBootApplication.java
@@ -2,10 +2,17 @@ package com.spring.boot.aws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestSpringBootApplication {
 
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome AWS deploy Sprinboot!!.." ;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TestSpringBootApplication.class, args);
 		System.out.println("Hello world");
